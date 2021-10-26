951b501437dc5023f53a848837928e126f64bb76
Edward Palingayan <edward.palingayan@home-search.co.jp>
2021-10-26 18:56:11 +0900

    Added taiwind

--- src/components/atoms/Buttons/index.spec.ts
+++ src/components/atoms/Buttons/index.spec.ts
@@ -1,9 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
 import Buttons from "@/components/atoms/Buttons/index.vue";
 
-describe("AtomButton.vue", () => {
-  it("renders props.msg when passed", () => {
-    const label = "new message";
+describe("Atom -> Button", () => {
+  it("renders props.label when passed", () => {
+    const label = "Submit";
     const wrapper = shallowMount(Buttons, {
       props: { label },
     });
